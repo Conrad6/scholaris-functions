773a04314749216822c88d106bee2c4485f723e1
Conrad Bekondo <philllittle302@gmail.com>
2024-04-27 14:08:14 +0100

    chore: add activities-updater function

---
+++ activities-updater/src/main.js
@@ -0,0 +1,14 @@
+import { Client, Databases } from 'node-appwrite';
+
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }) => {
+  // Why not try the Appwrite SDK?
+  //
+  const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+  const db = new Databases(client);
+  return res.send();
+};
