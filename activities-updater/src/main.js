77e725ed029a34ff86f3ae81b81cce126489a9c2
Conrad Bekondo <philllittle302@gmail.com>
2024-04-27 23:28:05 +0100

    attempt 1 again

--- activities-updater/src/main.js
+++ activities-updater/src/main.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from 'node-appwrite';
+import { Client, Databases, ID} from 'node-appwrite';
 
 // This is your Appwrite function
 // It's executed each time we get a request
@@ -6,9 +6,19 @@ export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
   const client = new Client()
-    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setEndpoint('https://api.scholaris.space/v1')
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setKey(process.env.API_KEY);
   const db = new Databases(client);
+
+  log(JSON.stringify(process.env));
+
+  try {
+    // await db.createDocument(process.env.DB_ID, process.env.COLLECTION_ID, ID.unique(), {
+
+    // })
+  } catch (err) {
+    error(err);
+  }
   return res.send();
 };
