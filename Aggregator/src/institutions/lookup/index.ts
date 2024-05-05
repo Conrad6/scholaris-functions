517765f1357fbe76c23fb8d0d5c3022aad886f76
Conrad Bekondo <philllittle302@gmail.com>
2024-05-05 12:31:44 +0100

    refactor: add roles to lookup institutions result schema

--- Aggregator/src/institutions/lookup/index.ts
+++ Aggregator/src/institutions/lookup/index.ts
@@ -1,4 +1,4 @@
-import { Databases, Models, Query, Teams } from "node-appwrite";
+import { Databases, Models, Query, Role, Teams } from "node-appwrite";
 import { RequestContext } from "../../models";
 const dbId = "6587eefbaf2d45dc4407";
 const institutionCollectionId = "659074c14a88d2072f38";
@@ -17,19 +17,21 @@ export async function get({ client, user }: RequestContext) {
     >(dbId, institutionCollectionId, filters);
 
     if (total == 0) return [];
-    const result = Array<Models.Document & { isSubscribed: boolean, engagementScore: number }>();
+    const result = Array<Models.Document & { roles: string[], isSubscribed: boolean, engagementScore: number }>();
 
     for (const document of documents) {
         let isSubscribed = false;
+        let roles = Array<string>();
         if (!!user) {
-            const { total } = await teams.listMemberships(document.$id, [
+            const { total, memberships } = await teams.listMemberships(document.$id, [
                 Query.equal("userId", user.$id),
             ]);
+            roles = [...new Set(memberships.flatMap(m => m.roles))];
             isSubscribed = total > 0;
         }
 
         if (isSubscribed || (document.isLive && document.visible))
-            result.push({ ...document, isSubscribed, engagementScore: 0 });
+            result.push({ ...document, isSubscribed, engagementScore: 0, roles });
     }
     return result;
 }
