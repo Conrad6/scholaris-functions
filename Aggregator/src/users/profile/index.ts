e592a017b0ee23ab0bba93d6394e6cb63122b64b
Conrad Bekondo <philllittle302@gmail.com>
2024-05-08 09:14:23 +0100

    update aggregator function

--- Aggregator/src/users/profile/index.ts
+++ Aggregator/src/users/profile/index.ts
@@ -1,5 +1,5 @@
-import { Users } from "node-appwrite";
-import { NotFoundException, RequestContext, UserPreferences } from "../../models";
+import { Databases, Query, Users } from "node-appwrite";
+import { NotFoundException, RequestContext, ScheduledOperation, UserPreferences } from "../../models";
 
 function maskText(text: string) {
     const lengthToMask = Math.floor(text.length * 0.5);
@@ -16,13 +16,26 @@ function maskText(text: string) {
     return maskedText;
 }
 
+const dbId = String(Bun.env['MAIN_DB_ID']);
+const schedulesCollectionId = 'schedules';
+
 export async function GET({ client, requestURL, user: principal }: RequestContext) {
     const users = new Users(client);
+    const db = new Databases(client);
+
     const userParam = requestURL.searchParams.get('id');
     if (!userParam) throw new NotFoundException(userParam ?? 'User ID');
 
     const foundUser = await users.get<UserPreferences>(userParam);
     if (!foundUser) throw new NotFoundException(userParam ?? 'User ID');
+
+    const scheduledForDeletion = await db.listDocuments<ScheduledOperation>(dbId, schedulesCollectionId, [
+        Query.equal('resource', `user:${foundUser.$id}`),
+        Query.notEqual('cancelled', true),
+        Query.isNull('executedAt')
+    ]).then(({ total }) => total > 0);
+
+    if (scheduledForDeletion) throw new NotFoundException(userParam ?? 'User ID');
 
     const { $id, email, name, phone, emailVerification, phoneVerification, prefs } = foundUser;
 
